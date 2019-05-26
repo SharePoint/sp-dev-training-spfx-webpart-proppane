bb69125591dc4e6cfa9728d5f6d35206db66f9d4
Rob Windsor <rob.windsor@gmail.com>
2019-05-26 13:28:16 -0400

    Update project files for Ex 2

--- Demos/02-custom-fields/src/controls/PropertyPaneContinentSelector/IPropertyPaneContinentSelectorProps.ts
+++ Demos/02-custom-fields/src/controls/PropertyPaneContinentSelector/IPropertyPaneContinentSelectorProps.ts
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
 import { IDropdownOption } from 'office-ui-fabric-react/lib/components/Dropdown';
 
 export interface IPropertyPaneContinentSelectorProps {
--- Demos/02-custom-fields/src/webparts/helloPropertyPane/HelloPropertyPaneWebPart.ts
+++ Demos/02-custom-fields/src/webparts/helloPropertyPane/HelloPropertyPaneWebPart.ts
@@ -1,27 +1,24 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
 import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import {
-  BaseClientSideWebPart,
   IPropertyPaneConfiguration,
   PropertyPaneTextField,
   PropertyPaneSlider
-} from '@microsoft/sp-webpart-base';
+} from '@microsoft/sp-property-pane';
 import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './HelloPropertyPaneWebPart.module.scss';
+import * as strings from 'HelloPropertyPaneWebPartStrings';
 
 import {
   PropertyPaneContinentSelector,
   IPropertyPaneContinentSelectorProps
 } from '../../controls/PropertyPaneContinentSelector';
 
-import styles from './HelloPropertyPaneWebPart.module.scss';
-import * as strings from 'HelloPropertyPaneWebPartStrings';
-
 export interface IHelloPropertyPaneWebPartProps {
   description: string;
   myContinent: string;
-  numContinentsVisited: number;  
+  numContinentsVisited: number;
 }
 
 export default class HelloPropertyPaneWebPart extends BaseClientSideWebPart<IHelloPropertyPaneWebPartProps> {
@@ -53,7 +50,7 @@ export default class HelloPropertyPaneWebPart extends BaseClientSideWebPart<IHel
   private validateContinents(textboxValue: string): string {
     const validContinentOptions: string[] = ['africa', 'antarctica', 'asia', 'australia', 'europe', 'north america', 'south america'];
     const inputToValidate: string = textboxValue.toLowerCase();
-
+  
     return (validContinentOptions.indexOf(inputToValidate) === -1)
       ? 'Invalid continent entry; valid options are "Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", and "South America"'
       : '';
@@ -63,7 +60,7 @@ export default class HelloPropertyPaneWebPart extends BaseClientSideWebPart<IHel
     const oldValue: any = this.properties[propertyPath];
     this.properties[propertyPath] = newValue;
     this.render();
-  }  
+  }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
@@ -81,18 +78,18 @@ export default class HelloPropertyPaneWebPart extends BaseClientSideWebPart<IHel
                 }),
                 // PropertyPaneTextField('myContinent', {
                 //   label: 'Continent where I currently reside',
-                //   onGetErrorMessage: this.validateContinents.bind(this)                  
+                //   onGetErrorMessage: this.validateContinents.bind(this)
                 // }),
                 new PropertyPaneContinentSelector('myContinent', <IPropertyPaneContinentSelectorProps>{
                   label: 'Continent where I currently reside',
                   disabled: false,
                   selectedKey: this.properties.myContinent,
                   onPropertyChange: this.onContinentSelectionChange.bind(this),
-                }),                
+                }),
                 PropertyPaneSlider('numContinentsVisited', {
                   label: 'Number of continents I\'ve visited',
-                  min: 1, max: 6, showValue: true,
-                })                  
+                  min: 1, max: 7, showValue: true,
+                })                       
               ]
             }
           ]
