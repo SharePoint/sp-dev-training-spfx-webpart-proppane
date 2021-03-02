b9a653856095a61fe30e1df643ed9741857715e4
Rob Windsor <rob.windsor@gmail.com>
2021-03-02 05:58:27 -0500

    FY21Q3 content refresh
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

--- Demos/01-proppane/src/webparts/helloPropertyPane/HelloPropertyPaneWebPart.ts
+++ Demos/01-proppane/src/webparts/helloPropertyPane/HelloPropertyPaneWebPart.ts
@@ -23,17 +23,17 @@ export default class HelloPropertyPaneWebPart extends BaseClientSideWebPart<IHel
 
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.helloPropertyPane}">
-        <div class="${ styles.container}">
-          <div class="${ styles.row}">
-            <div class="${ styles.column}">
-              <span class="${ styles.title}">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description}">${escape(this.properties.description)}</p>
-              <p class="${ styles.description}">Continent where I reside: ${escape(this.properties.myContinent)}</p>
-              <p class="${ styles.description}">Number of continents I've visited: ${this.properties.numContinentsVisited}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button}">
-                <span class="${ styles.label}">Learn more</span>
+      <div class="${styles.helloPropertyPane}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <span class="${styles.title}">Welcome to SharePoint!</span>
+              <p class="${styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${styles.description}">${escape(this.properties.description)}</p>
+              <p class="${styles.description}">Continent where I reside: ${escape(this.properties.myContinent)}</p>
+              <p class="${styles.description}">Number of continents I've visited: ${this.properties.numContinentsVisited}</p>              
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
               </a>
             </div>
           </div>
